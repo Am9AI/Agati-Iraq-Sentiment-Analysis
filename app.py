3161ee71f66d7334a7deee27fd12015cf65f33e8
Amna A. Alrawi <amna.ahmed.2004928@gmail.com>
2026-06-26 21:41:36 -0700

    Initialize Streamlit app with page configuration

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(
+    page_title="Agati Iraq Dashboard",
+    page_icon="🚖",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
